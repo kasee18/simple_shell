c3b58cf05678d095c148d9ba24b070e12eec636a
kasee18 <mutisyavincent99@gmail.com>
2023-11-15 18:49:29 -0800

    git commit for shell.c file

--- shell.h
+++ shell.h
@@ -1,21 +1,31 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/**
- * Below are the function headers
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 /**
- * The prototypes for all the functions in the project
- * kasee_print - a function that enables the user to print
- * display_prompt - this function displays a prompt
+ * kasee_print - a function that prints a string
+ * @str: string to be printed
  */
-
 void kasee_print(const char *str);
+/**
+ * display_prompt - a function that displays shell promt
+ */
 void display_prompt(void);
+/**
+ * real_cmd - a funct that reads a command from the user
+ * @cmd: buffer to store command
+ * @size: buffer size
+ */
+void real_cmd(char *cmd, size_t size);
+/**
+ * execute_command -executes a command in shell
+ * @command: command to be executed
+ */
+void execute_command(const char *command);
 int main(int ac, char **argv);
+int main(void);
 
 #endif
