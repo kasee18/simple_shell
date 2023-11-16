04013a3adca3645273d182ae654717be303a4ee4
kasee18 <mutisyavincent99@gmail.com>
2023-11-15 16:24:26 -0800

    git commiting files

--- execv.c
+++ execv.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
- * execute_cmd - a function that executes files
+ * execute_command - a function that executes files
  * @cmd: argument
  * Return: void
  */
 
-void execute_cmd(const char *cmd)
+void execute_command(const char *command)
 {
-	char *cmd[] = {"/bin/ls", "-1", NULL};
+	char command[] = {"/bin/ls", "-1", NULL};
 	pid_t child_pid = fork();
 
 	if (child_pid == -1)
@@ -19,9 +19,11 @@ void execute_cmd(const char *cmd)
 	}
 	else if (child_pid == 0)
 	{
-		if execve(cmd[0], cmd, == NULL)
+		if (execve(command[0], command, == NULL) == -1)
+		{
 		perror("execve");
 		exit(EXIT_FAILURE);
+		}
 	}
 	else
 	{
